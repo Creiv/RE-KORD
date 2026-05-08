66b73db5c6095def79a261ddda87995c7c6d3569
Diego Cravero <diego.cravero@ujiboo.com>
2026-05-08 22:46:58 +0200

    delete old card+ better discover page

--- src/components/library/index.ts
+++ src/components/library/index.ts
@@ -4,3 +4,11 @@ export { ArtistListTile } from "./ArtistListTile";
 export type { ArtistListTileProps } from "./ArtistListTile";
 export { GenreListTile } from "./GenreListTile";
 export type { GenreListTileProps } from "./GenreListTile";
+export {
+  StudioCatalogAlbumTile,
+  StudioCatalogArtistTile,
+} from "./StudioCatalogTiles";
+export type {
+  StudioCatalogAlbumTileProps,
+  StudioCatalogArtistTileProps,
+} from "./StudioCatalogTiles";
